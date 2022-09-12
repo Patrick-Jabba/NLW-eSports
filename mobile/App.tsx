c2509d7d65f02b8ef049dc3de7a1763d2b6fdc29
Patrick Monteiro Fischer <patrickmf.dsn.pack@alterdata.matriz>
2022-09-12 12:24:45 -0300

    inicialização do mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+
+      <Button title='Send1'></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+      {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 28
+  }
+});
